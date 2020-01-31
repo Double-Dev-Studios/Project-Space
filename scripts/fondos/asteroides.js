760b036acfb30851b498c26ac2db4aae7f9e5590
Manulito1516 <60412060+Manulito1516@users.noreply.github.com>
2020-01-31 12:47:55 -0300

    arreglo de bugs y avance de niveles

---
+++ scripts/fondos/asteroides.js
@@ -0,0 +1,33 @@
+//fondo
+					
+// fondos
+	var bgAsteroides = new Image()
+	bgAsteroides.src = "bg/asteroides.png"
+
+// variables de fondo
+	var actualBG = bgAsteroides
+	var actualBGString = "bgAsteroides"
+	
+	var bg = {
+		offset: 0,
+		speed: 0
+	}
+				
+// funciones de fondo
+	function dibujarFondo(){
+		ctx.drawImage(actualBG, bg.offset, 0,)
+	}
+						
+	function actualizarFondo(){
+		if (ship.speed == 1.5) {
+			bg.offset -= bg.speed
+		}
+		
+		if (ship.speed == 3) {
+			bg.offset -= (bg.speed * 2)
+		}
+		
+		if (ship.speed >= 4.5) {
+			bg.offset -= (bg.speed * 3)
+		}
+	}
