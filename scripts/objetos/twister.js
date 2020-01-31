21b1e517b47f6c7612aa1e02403e94218ba9e7de
Mati-coder <60388900+Mati-coder@users.noreply.github.com>
2020-01-31 09:45:19 -0300

    Add files via upload

--- scripts/objetos/twister.js
+++ scripts/objetos/twister.js
@@ -4,7 +4,7 @@
 
 var sprTwister = new Image()
 var sprAlert = new Image()
-sprTwister.src = "sprites/twister(Prueba)/twister.png"
+sprTwister.src = "sprites/twister.png"
 sprAlert.src = "sprites/lava/lava0.png"
 var sprTwisterN = sprTwister
 
@@ -15,14 +15,14 @@ var contadorT2 = true
 
 var twister = {
     x: getRandomInt(64, canvas.width - 64),
-    y: canvas.height - (64 * gameScale),
+    y: canvas.height - 20 * gameScale,
     speedX: 1.5,
     speedY: 0.75,
     width: 20 * gameScale,
     height: 20 * gameScale,
     show: true,
     use: true,
-    alert: false,
+    alert: true,
     direction: "left"
 }
 
@@ -35,25 +35,31 @@ function dibujarTwister(){
 }
 
 function actualizarTwister(){
-    if(twister.use == true){
-        twister.y -= twister.speedY
-        contadorT -= getRandomInt(1, 2)
-        if(twister.direction == "left"){
-            twister.x -= twister.speed
-        }
-        else{
-            twister.x += twister.speed
-        }
+    if(twister.use){
+        if(alert){
+            if(twister.y <= 0){
+                twister.y = canvas.height
+            }
+            twister.y = twister.y - twister.speedY
 
-        if(contadorT <= 0){
-            contadorT = 50
-            if(contadorT2){
-                twister.direction = "right"
-                contadorT2 = false
+            if(twister.direction == "left"){
+                twister.x = twister.x - twister.speedX
             }
             else{
-                twister.direction = "left"
-                contadorT2 = true
+                twister.x = twister.x + twister.speedX
+            }
+
+            contadorT = contadorT - getRandomInt(1, 2)
+            if(contadorT <= 0){
+                contadorT = 50
+                if(contadorT2){
+                    twister.direction = "right"
+                    contadorT2 = false
+                }
+                else{
+                    twister.direction = "left"
+                    contadorT2 = true
+                }
             }
         }
     }
