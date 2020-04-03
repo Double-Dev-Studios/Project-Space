97d248d78a8b605178adb5bf7f3fbb2bd95eaf38
Manulito1516 <60412060+Manulito1516@users.noreply.github.com>
2020-04-03 11:44:51 -0300

    Neptune the new level

---
+++ scripts/fondos/neptuno.js
@@ -0,0 +1,26 @@
+//fondo
+					
+// fondos
+	var bgNeptuno = new Image()
+	bgNeptuno.src = "bg/neptuno.png"
+
+// variables de fondo
+	var actualBG = bgNeptuno
+		
+		var actualBGString = "bgNeptuno"
+	
+	var bg = {
+		offset: 400,
+		speed: 0.09,
+		minTemp: -50,
+		tempAdd: -0.09
+	}
+	
+// funciones de fondo
+	function dibujarFondo(){
+		ctx.drawImage(actualBG, bg.offset, 0,)
+	}
+						
+	function actualizarFondo(){
+		bg.offset -= (ship.speed / 2 * bg.speed)
+	}
